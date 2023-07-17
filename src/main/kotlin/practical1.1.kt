a88a30b9dd5394a20c42e92ab12a7184f8448a7c
student <student@gnu.ac.in>
2023-07-17 10:39:22 +0530

    done

--- src/main/kotlin/practical1.1.kt
+++ src/main/kotlin/practical1.1.kt
@@ -1,4 +1,23 @@
 fun main()
 {
-    println("Hello World")
+   var a=10
+    println("$a is integer variable")
+
+    var b = "Hello"
+    println("$b is string variable")
+
+    var c = 15.30f
+    println("$c is double variable")
+
+    var d = 'p'
+    println("$d is character variable")
+
+    var e =true
+    println("$e is boolian variable")
+
+
+
+
+
+
 }
