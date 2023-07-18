5f79addc5f7047cf6fa2f8aa3a472ba5599d6373
student <student@gnu.ac.in>
2023-07-18 09:20:29 +0530

    practical1.2.kt

---
+++ src/main/kotlin/practical1.2.kt
@@ -0,0 +1,12 @@
+
+
+    fun main(){
+        var a : Int = 5
+        var b : Double = 5.479
+        var c : String = "74"
+
+        println("Integer Value : $a")
+        println("Double Value : ${a.toDouble()}")        // Integer to Double
+        println("String Value : ${b.toString()}")        // Double to String
+        println("Integer Value : ${c.toInt()}")          // String to Integer
+    }
